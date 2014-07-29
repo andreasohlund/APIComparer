e97ef23a70ab9616a3dec6ff13008cefee41b8d1
Cameron MacFarland <distantcam@gmail.com>
2014-07-29 12:14:40 +0800

    Simplified output

--- APIComparer.Core/Outputters/APIUpgradeToMarkdownFormatter.cs
+++ APIComparer.Core/Outputters/APIUpgradeToMarkdownFormatter.cs
@@ -21,8 +21,8 @@ namespace APIComparer.Outputters
             var sb = new StringBuilder();
 
             var missingTypes = MissingMembers(diff.LeftOrphanTypes, diff.MatchingTypeDiffs.Select(td => Tuple.Create(td.LeftType, td.RightType)))
-                .Select(t => t.FullName)
-                .OrderBy(s => s);
+                .OrderBy(t => t.FullName)
+                .Select(FormatType);
 
             sb.AppendLine("The following types are missing in the new API.");
             sb.AppendLine();
@@ -45,12 +45,12 @@ namespace APIComparer.Outputters
 
         private void WriteOut(TypeDiff typeDiff, StringBuilder sb)
         {
-            var missingFields = MissingMembers(typeDiff.LeftOrphanFields, typeDiff.MatchingFields).Select(f => f.FullName).OrderBy(s => s);
-            var missingMethods = MissingMembers(typeDiff.LeftOrphanMethods, typeDiff.MatchingMethods).Select(f => f.FullName).OrderBy(s => s);
+            var missingFields = MissingMembers(typeDiff.LeftOrphanFields, typeDiff.MatchingFields).OrderBy(f => f.Name).Select(FormatField);
+            var missingMethods = MissingMembers(typeDiff.LeftOrphanMethods, typeDiff.MatchingMethods).OrderBy(m => m.Name).Select(FormatMethod);
 
             if (missingFields.Any() || missingMethods.Any())
             {
-                sb.AppendLine("    " + typeDiff.LeftType.FullName);
+                sb.AppendLine("    " + FormatType(typeDiff.LeftType));
 
                 foreach (var missingField in missingFields)
                 {
@@ -70,6 +70,59 @@ namespace APIComparer.Outputters
         {
             return members
                 .Concat(matching.Select(t => t.Item1));
+        }
+
+        private string FormatField(FieldDefinition field)
+        {
+            return FormatType(field.FieldType) + " " + field.Name;
+        }
+
+        private string FormatMethod(MethodDefinition method)
+        {
+            var methodParams = method.Parameters.Select(p => FormatType(p.ParameterType));
+
+            return FormatType(method.ReturnType) + " " + method.Name + "(" + string.Join(", ", methodParams) + ")";
+        }
+
+        private string FormatType(TypeReference type)
+        {
+            var name = type.Namespace + "." + type.Name;
+            var genericParams = Enumerable.Empty<string>();
+
+            if (type.IsArray)
+                name = name.Substring(0, name.Length - 2);
+
+            if (name == "System.Void")
+                name = "void";
+            if (name == "System.Boolean")
+                name = "bool";
+            if (name == "System.String")
+                name = "string";
+            if (name == "System.Int32")
+                name = "int";
+            if (name == "System.Object")
+                name = "object";
+
+            var genericType = type as GenericInstanceType;
+            if (genericType != null)
+            {
+                name = type.FullName.Split('`')[0];
+                genericParams = genericType.GenericArguments.Select(FormatType);
+            }
+
+            if (type.HasGenericParameters)
+            {
+                name = type.FullName.Split('`')[0];
+                genericParams = type.GenericParameters.Select(FormatType);
+            }
+
+            if (genericParams.Any())
+                name = name + "<" + string.Join(", ", genericParams) + ">";
+
+            if (type.IsArray)
+                name = name + "[]";
+
+            return name;
         }
     }
 }

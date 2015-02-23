97ecaadf9776b01760543ec12ab68b20b98f9a30
Andreas Öhlund <andreas.ohlund@particular.net>
2015-02-23 21:09:46 +0100

    Detecting types made non public as breaking change

--- APIComparer.Tests/BreakingChange.cs
+++ APIComparer.Tests/BreakingChange.cs
@@ -1,19 +1,34 @@
-﻿using Mono.Cecil;
+﻿using APIComparer;
+using Mono.Cecil;
 
 public class BreakingChange
 {
-    public TypeDefinition TypeDefinition { get; set; }
-
-    public string Reason { get; set; }
-
-    internal BreakingChange(TypeDefinition typeDefinition)
+    public static BreakingChange TypeRemoved(TypeDefinition typeDefinition)
     {
-        TypeDefinition = typeDefinition;
-        Reason = "Type removed";
+        return new BreakingChange("Type removed", typeDefinition.ToString());
     }
+    public static BreakingChange TypeMadeNonPublic(TypeDiff typeDiff)
+    {
+        return new BreakingChange("Type made non public", typeDiff.RightType.ToString());
+    }
+    
+
+    BreakingChange(string reason,string changedThing)
+    {
+        this.reason = reason;
+        this.changedThing = changedThing;
+    }
+
+
+     
+
 
     public override string ToString()
     {
-        return string.Format("{0}: {1}", Reason, TypeDefinition);
+        return string.Format("{0}: {1}", reason, changedThing);
     }
+
+ readonly string reason;
+    readonly string changedThing;
+
 }
--- APIComparer.Tests/BreakingChangeFinder.cs
+++ APIComparer.Tests/BreakingChangeFinder.cs
@@ -6,7 +6,11 @@ public static class BreakingChangeFinder
 {
     public static IEnumerable<BreakingChange> Find(Diff diff)
     {
-        var result = diff.RemovedPublicTypes().Select(t => new BreakingChange(t));
+        var result = diff.RemovedPublicTypes().Select(BreakingChange.TypeRemoved)
+            .ToList();
+
+        result.AddRange(diff.TypesChangedToNonPublic()
+            .Select(BreakingChange.TypeMadeNonPublic));
 
         return result;
     }

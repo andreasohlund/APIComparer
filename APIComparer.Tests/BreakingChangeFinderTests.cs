995ddd6c7d4efcbefa8afc31ffe6e7776b2b82ef
Simon Cropp <simon.cropp@gmail.com>
2015-03-04 22:44:11 +1100

    suppress some errors

--- APIComparer.Tests/BreakingChangeFinderTests.cs
+++ APIComparer.Tests/BreakingChangeFinderTests.cs
@@ -11,11 +11,8 @@ class BreakingChangeFinderTests
     [Test]
     public void ApproveExample()
     {
-        var file1 = "ExampleV1.dll";
-        var file2 = "ExampleV2.dll";
-
         var engine = new ComparerEngine();
-        var diff = engine.CreateDiff(file1, file2);
+        var diff = engine.CreateDiff("ExampleV1.dll", "ExampleV2.dll");
 
         var breakingChanges = BreakingChangeFinder.Find(diff);
         Approvals.Verify(string.Join(Environment.NewLine,breakingChanges));

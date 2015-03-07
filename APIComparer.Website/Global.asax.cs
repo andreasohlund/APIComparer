613dfed928b0a01b8e9491f8a4bac11be7b29444
danielmarbach <daniel.marbach@openplace.net>
2015-03-07 14:02:50 +0100

    Setting up DI to wire controllers

--- APIComparer.Website/Global.asax.cs
+++ APIComparer.Website/Global.asax.cs
@@ -1,21 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace APIComparer.Website
+﻿namespace APIComparer.Website
 {
-    public class MvcApplication : System.Web.HttpApplication
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using Autofac;
+    using Autofac.Integration.Mvc;
+    using NServiceBus;
+    using NServiceBus.Features;
+
+    public class MvcApplication : HttpApplication
     {
+        IStartableBus startableBus;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var configuration = new BusConfiguration();
+            WireupAutofacContainer(configuration);
+
+            configuration.UseTransport<AzureStorageQueueTransport>();
+            configuration.UsePersistence<AzureStoragePersistence>();
+            configuration.DisableFeature<SecondLevelRetries>();
+            configuration.DisableFeature<Sagas>();
+            configuration.DisableFeature<TimeoutManager>();
+
+            startableBus = Bus.Create(configuration);
+            startableBus.Start();
+        }
+
+        private static void WireupAutofacContainer(BusConfiguration configuration)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly)
+                .PropertiesAutowired();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            configuration.UseContainer<AutofacBuilder>(x => x.ExistingLifetimeScope(container));
+        }
+
+        protected void Application_End()
+        {
+            startableBus.Dispose();
         }
     }
 }

b9b5744c379a8a387ea58ada1f1e077a5eefe6a0
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-13 15:22:19 +0100

    Adding octopack and gitversiontask

--- APIComparer.Website/Properties/AssemblyInfo.cs
+++ APIComparer.Website/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("300ad14b-29fb-48ce-91a0-53873d4973e8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

c930058e146ac23da51a9f0ac7ee4e19c56d5ebc
danielmarbach <daniel.marbach@openplace.net>
2015-04-30 15:30:04 +0200

    Codify properly implemented

--- APIComparer.Backend/Reporting/Helpers.Html.cs
+++ APIComparer.Backend/Reporting/Helpers.Html.cs
@@ -2,18 +2,22 @@ namespace APIComparer.Backend.Reporting
 {
     using System;
     using System.IO;
+    using System.Text.RegularExpressions;
     using HandlebarsDotNet;
 
     public class Helpers_Html
     {
+        private static readonly Regex codify = new Regex(@"`(?<x>[\w\.\(\)]*)`", RegexOptions.Compiled);
+
         public static void Codify(TextWriter writer, dynamic context, params object[] args)
         {
             if (args.Length != 1)
             {
                 throw new InvalidOperationException("Codify helper only supports one argument");
             }
-            // TODO
-            writer.WriteSafeString(args[0]);
+
+            var input = args[0] as string ?? string.Empty;
+            writer.WriteSafeString(codify.Replace(input, @"<code>${x}</code>"));
         }
     }
 }

ec7d7a9529062fa96b1de27500d59d160c4a6d8e
andreasohlund <andreas.ohlund@particular.net>
2015-09-05 10:21:39 +0200

    Add type to expose issue with obsoletes always showing

---
+++ ExampleV2/ClassObsoletedInBothVersions.cs
@@ -0,0 +1,9 @@
+namespace Example
+{
+    using System;
+
+    [Obsolete("FieldToBeRemoved. Will be removed in version 3.0.0.", true)]
+    public class ClassObsoletedInBothVersions
+    {
+    }
+}
